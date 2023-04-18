9584c4751b9f0364cde30ee3ab30ed3708c41e7e
codemonkey721 <code.monkey721@gmail.com>
2023-04-18 03:19:41 -0700

    Update Leads

--- src/__pages/Leads/DataTable.jsx
+++ src/__pages/Leads/DataTable.jsx
@@ -1,43 +1,107 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import DataTable from "react-data-table-component";
-import { dummytabledata, tableColumns } from "./const";
-import { Card } from "reactstrap";
+import { Button, ButtonGroup } from "reactstrap";
+import { fetch_leads, get_lead } from "../../redux/actions/lead";
+import { tableColumns } from "./const";
+import { Btn } from "../../AbstractElements";
+import CustomePagination from "../../__components/CustomePagination";
+import EditModal from "./EditModal";
+
+const moment = require("moment");
 
 const Table = () => {
-  let temp = [];
+  const dispatch = useDispatch();
 
-  for (let i = 0; i < 20; i++) {
-    temp.push(dummytabledata);
+  const [modal, setModal] = useState(false);
+  const searchParams = new URL(window.location.href).searchParams;
+
+  const filter = {};
+
+  for (const key of searchParams.keys()) {
+    filter[key] = searchParams.get(key);
   }
+
+  const toggle = () => {
+    setModal(!modal);
+  };
+
+  useEffect(() => {
+    dispatch(fetch_leads(filter));
+  }, []);
+
+  const { leads, cnt } = useSelector((state) => state.lead);
+  const { user } = useSelector((state) => state.auth);
+
+  let data = [];
+  leads.map((item) => {
+    data.push({
+      country: item.country,
+      description: item.description,
+      emailn: item.emailn,
+      seller: item.seller,
+      price: `$ ${item.price}`,
+      added_on: moment(item.date).format("yyyy.MM.DD hh:mm:ss A"),
+      action: (
+        <div className="btn-group-showcase">
+          <ButtonGroup
+            className="btn-group-pill"
+            style={{ display: "contents" }}
+          >
+            <Btn
+              attrBtn={{
+                size: "sm",
+                className: "p-2",
+                color: "success",
+                outline: true,
+              }}
+            >
+              <i className="fa fa-paper-plane-o"></i>
+            </Btn>
+            {user && user.role === "admin" ? (
+              <Button
+                size="sm"
+                className="p-2"
+                color="info"
+                outline={true}
+                onClick={() => {
+                  dispatch(get_lead(item));
+                  toggle(item);
+                }}
+              >
+                <i className="fa fa-edit"></i>
+              </Button>
+            ) : (
+              <Btn
+                attrBtn={{
+                  size: "sm",
+                  className: "p-2",
+                  color: "info",
+                  outline: true,
+                  onClick: toggle(item),
+                }}
+              >
+                <i className="fa fa-shopping-cart"></i>
+              </Btn>
+            )}
+          </ButtonGroup>
+        </div>
+      ),
+    });
+  })
 
   return (
     <Fragment>
-      <Card className="shadow-lg shadow-showcase">
-        <DataTable
-          data={temp}
-          columns={tableColumns}
-          striped={true}
-          center={false}
-          pagination
-          responsive={true}
-          highlightOnHover
-          customStyles={{
-            headCells: {
-              style: {
-                whiteSpace: "pre",
-                justifyContent: "center",
-                // padding: "0px 2px",
-              },
-            },
-            cells: {
-              style: {
-                // padding: "0px 2px!important",
-                whiteSpace: "pre!important",
-              },
-            },
-          }}
-        />
-      </Card>
+      <DataTable
+        data={data}
+        columns={tableColumns}
+        striped={true}
+        center={false}
+        responsive={true}
+      />
+      <hr className="mt-4 mb-4" />
+      <CustomePagination cnt={cnt} func={fetch_leads} />
+      <EditModal isOpen={modal} title={"Edit"} toggler={toggle} />
     </Fragment>
   );
 };
--- src/__pages/Leads/const.jsx
+++ src/__pages/Leads/const.jsx
@@ -1,69 +1,48 @@
-import React from 'react';
-import { Btn } from '../../AbstractElements';
-import { Country, Description, EmailN, Buy, Seller, Addedon, Price, Action } from '../../Constant';
-import { ButtonGroup } from 'reactstrap';
-
-export const dummytabledata = 
-{
-    country: <span><i className="flag-icon flag-icon-us"></i>United States</span>,
-    description: '✔️ DATABASE Singapour - 2K23✔️ - 20K✔️ - FULL DATA ✔️ https://prnt.sc/MhUol36zyd-k',
-    emailn: '20K',
-    seller: 'seller20',
-    price: '$ 10',
-    added_on: "2023/04/03 02:14:52 PM",
-    action: <div className="btn-group-showcase">
-                <ButtonGroup className='btn-group-pill' style={{ display: 'contents' }}>
-                    <Btn attrBtn={{  size: 'sm', className: 'p-2', color: 'success', outline: true }} ><i className="fa fa-edit"></i></Btn>
-                    <Btn attrBtn={{  size: 'sm', className: 'p-2', color: 'info', outline: true }} ><i className="fa fa-shopping-cart"></i></Btn>
-                </ButtonGroup>
-            </div>,
-};
-
 export const tableColumns = [
     {
-        name: `${Country}`,
+        name: "Country",
         selector: row => row['country'],
         sortable: false,
         center: false,
         width: '10%'
     },
     {
-        name: `${Description}`,
-        selector: row => `${row.description}`,
+        name: "Description",
+        selector: row => row.description,
         sortable: false,
         center: false,
         width: '40%'
     },
     {
-        name: `${EmailN}`,
-        selector: row => `${row.emailn}`,
+        name: "EmailN",
+        selector: row => row.emailn,
         sortable: false,
         center: false,
         width: '10%'
     },
     {
-        name: `${Seller}`,
+        name: "Seller",
         selector: row => row.seller,
         sortable: false,
         center: false,
         width: '10%'
     },
     {
-        name: `${Price}`,
+        name: "Price",
         selector: row => row.price,
         sortable: false,
         center: false,
         width: '5%'
     },
     {
-        name: `${Addedon}`,
+        name: "Added on",
         selector: row => row.added_on,
         sortable: false,
         center: false,
         width: '15%'
     },
     {
-        name: `${Action}`,
+        name: "Action",
         selector: row => row['action'],
         sortable: false,
         center: true,
